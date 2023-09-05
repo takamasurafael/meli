118c5c2e9b60d0815097c1c306287a3d15f22212
Rafael Moura <123047561+takamasurafael@users.noreply.github.com>
2023-09-04 22:55:17 -0300

    a

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function hideAllFrames() {
+  frameSeller.style.display = "none";
   frameDiferimento.style.display = "none";
-  frame4.style.display = "none";
-  frame5.style.display = "none";
+  frameFSR.style.display = "none";
   frame6.style.display = "none";
   frame7.style.display = "none";
   frame8.style.display = "none";
@@ -11,33 +11,33 @@ function hideAllFrames() {
 }
 
 // Vendedor - Post Compra Funcionalidades
-const nav4 = document.getElementById("nav4");
-const frame4 = document.getElementById("frame4");
+const seller = document.getElementById("seller");
+const frameSeller = document.getElementById("frameSeller");
 const link4 =
   "https://docs.google.com/presentation/d/1v4feoFH7cJHis5elol5IbnrwPR1WYMM0-x3a1nh-ZE8/edit#slide=id.g13ebedf89ed_341_0";
-nav4.addEventListener("click", () => {
+seller.addEventListener("click", () => {
   hideAllFrames();
-  frame4.style.display = "flex";
-  frame4.src = link4;
+  frameSeller.style.display = "flex";
+  frameSeller.src = link4;
 });
 
 // FSR
-const nav5 = document.getElementById("nav5");
-const frame5 = document.getElementById("frame5");
+const menuFSR = document.getElementById("menuFSR");
+const frameFSR = document.getElementById("frameFSR");
 const link5 =
   "https://docs.google.com/presentation/d/1WfNpp3rs8pTXPV0V4kMZ3wBXetA14exz1pZ26ssyWMc/edit#slide=id.p1";
-nav5.addEventListener("click", () => {
+menuFSR.addEventListener("click", () => {
   hideAllFrames();
-  frame5.style.display = "flex";
-  frame5.src = link5;
+  frameFSR.style.display = "flex";
+  frameFSR.src = link5;
 });
 
 // BUG
-const nav6 = document.getElementById("nav6");
+const menuBUG = document.getElementById("menuBUG");
 const frame6 = document.getElementById("frame6");
 const link6 =
   "https://docs.google.com/presentation/d/1YdGGQFKiVCKZh1gKpVyIbCW_C42CWS0DvHgvEtcQihw/edit#slide=id.g22d184d4d76_495_3597";
-nav6.addEventListener("click", () => {
+menuBUG.addEventListener("click", () => {
   hideAllFrames();
   frame6.style.display = "flex";
   frame6.src = link6;
